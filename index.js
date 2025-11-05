95cf9e465327eae31887a359e7899661332d846f
aal80 <aal80@users.noreply.github.com>
2025-11-05 14:00:14 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ export const handler = async (event) => {
   // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from test3'),
+    body: JSON.stringify('Hello from test4'),
   };
   return response;
 };
